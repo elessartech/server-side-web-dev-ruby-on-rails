3e421410fd26aef125cf19b44bb7c5a8051a0c61
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-09-26 02:07:21 +0300

    tehtävä 13 done

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,7 +3,7 @@ class Beer < ApplicationRecord
 end
   
 class Brewery < ApplicationRecord
-    has_many :beers
+    has_many :beers, dependent: :destroy
     def restart
         self.year = 2022
         puts "changed year to #{year}"
