2991d2c0dc28beb16ef455d7f2112770a5f91003
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-09-28 00:22:38 +0300

    9-10 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     include RatingAverage
     validates :username, uniqueness: true,
     length: { minimum: 3, maximum: 30 }
-    has_many :ratings   # käyttäjällä on monta ratingia
+    has_many :ratings
     has_many :beers, through: :ratings
+    has_many :beer_clubs, through: :memberships
+    has_many :memberships, dependent: :destroy
 end
---
+++ db/migrate/20220927201628_create_memberships.rb
@@ -0,0 +1,10 @@
+class CreateMemberships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :memberships do |t|
+      t.string :beer_club_id
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
