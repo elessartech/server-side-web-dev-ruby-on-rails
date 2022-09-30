1cd04b9aecf171d8a32bc309fe0b3e2d51072233
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-09-30 23:25:04 +0300

    task 4 | function favorite_brewery implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,9 @@ class User < ApplicationRecord
     return nil if ratings.empty?
     ratings.order(score: :desc).limit(1).first.beer.style
   end
+
+  def favorite_brewery
+    return nil if ratings.empty?
+    ratings.order(score: :desc).limit(1).first.beer.brewery.name
+  end
 end
