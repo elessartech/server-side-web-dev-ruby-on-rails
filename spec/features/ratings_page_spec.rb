941453c4ad6118568d40cc3a951cc01c9b34e5e6
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-10-01 17:48:55 +0300

    task 6 done

---
+++ spec/features/ratings_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Ratings page" do
+  describe "displays" do
+    it "no ratings when they are not created" do
+        visit ratings_path
+        expect(page).to have_content 'List of ratings'
+        expect(page).to have_content 'Number of ratings: 0'
+    end
+
+    it "ratings properly when they are created" do
+      user = FactoryBot.create(:user)
+      beer = FactoryBot.create(:beer)
+      FactoryBot.create(:rating, score: 20, beer: beer, user: user)
+      visit ratings_path
+      expect(page).to have_content 'List of ratings'
+      expect(page).to have_content 'Number of ratings: 1'
+      expect(page).to have_content 'anonymous 20'
+  end
+  end
+end
